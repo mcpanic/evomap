206781c1c17b046b2a32816cdf4fe45ff7a7351f
jradoo <jradoo@49258776-dd40-11dd-8561-ef618b9164eb>
2009-01-17 16:06:11 +0000

    added outline.txt

--- trunk/generation/list/main.cpp
+++ trunk/generation/list/main.cpp
@@ -10,20 +10,14 @@ float objective(MapGenome &);
 int main(int argc, char* argv[])
 {
 
-	// set default parameters
-
-	// Define Genome
 	MapGenome genome;
 
 	// Define GA
 	GASteadyStateGA ga(genome);
-	// remove crossover
-
 	ga.populationSize(10);
-  ga.nGenerations(10);
-	ga.pMutation(0.2);
-	ga.set(gaNpCrossover, 0.0);	
-
+    ga.nGenerations(10);
+	ga.pMutation(0.2)
+	ga.set(gaNpCrossover, 0.0);
 	// Evolve
 	ga.evolve();
 }
