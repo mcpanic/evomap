bc22e4af2603e9b1a4a7e6380d24c3c9c1ed5acd
jradoo <jradoo@49258776-dd40-11dd-8561-ef618b9164eb>
2009-01-17 17:02:19 +0000

    cleaned up, added guideline comments

--- generation/list/MapGenome.h
+++ generation/list/MapGenome.h
@@ -10,28 +10,14 @@ public:
 
 public:
 
-	MapGenome() {
-		initializer(Init);
-		mutator(Mutate);
-		evaluator(Evaluate);
-		crossover(Cross);
-	}
+	MapGenome();
+	MapGenome(const MapGenome& orig);	
+	virtual ~MapGenome();
 
-	MapGenome(const MapGenome& orig) { copy(orig); }
+	MapGenome& operator=(const GAGenome& orig);
+	virtual GAGenome* clone(CloneMethod) const;
+	virtual void copy(const GAGenome& orig);
 
-	virtual ~MapGenome() {}
-	MapGenome& operator=(const GAGenome& orig){
-		if(&orig != this) copy(orig);
-		return *this;
-	}
-
-	virtual GAGenome* clone(CloneMethod) const {return new MapGenome(*this);}
-
-	virtual void copy(const GAGenome& orig) {
-		// this copies all of the base genome parts
-		MapGenome& g = (MapGenome&)orig;
-		GAListGenome::copy(orig);
-	}
 
 	// Data/member functions specific to this new class
 private:
