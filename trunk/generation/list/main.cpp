075e49cfa0becdfd99b57b9b9fcdfe2cfef406c8
jradoo <jradoo@49258776-dd40-11dd-8561-ef618b9164eb>
2009-01-18 09:18:37 +0000

    now compiles ( mutation to be done)

--- trunk/generation/list/main.cpp
+++ trunk/generation/list/main.cpp
@@ -13,8 +13,8 @@ int main(int argc, char* argv[])
 	// Define GA
 	GASteadyStateGA ga(genome);
 	ga.populationSize(10);
-    ga.nGenerations(10);
-	ga.pMutation(0.2)
+	ga.nGenerations(10);
+	ga.pMutation(0.2);
 	ga.set(gaNpCrossover, 0.0);
 	// Evolve
 	ga.evolve();
