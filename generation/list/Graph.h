84038a4d72d0128ba8a21b345c931f4aeec43f99
jradoo <jradoo@49258776-dd40-11dd-8561-ef618b9164eb>
2009-01-23 05:12:14 +0000

    temporary commit

--- generation/list/Graph.h
+++ generation/list/Graph.h
@@ -4,7 +4,7 @@
 #include "LinkedList.h"
 #include "MapNode.h"
 
-
+/*
 class DirectedGraph {
 	public:
 		void addNode(int id);
@@ -17,19 +17,64 @@ class DirectedGraph {
 	private:
 		SortedLinkedList<MapNode> nodeList;
 };
-
+*/
 class UndirectedGraph {
 	public:
-		void addNode(int id) {
-		}
-		void addEdge(int fromid, int toid);
-		void deleteNode(int id);
-		void deleteNthNode(int n);
-		void deleteEdge(int fromid, int toid);
-		void deleteRandomEdge();
 
-	private:
+		MapNode* findNode(int id)
+		{
+			nodeList.find(id);
+		}
+
+		bool addNode(int id) {
+			return nodeList.insert(id);
+		}
+
+		bool addEdge(int fromid, int toid) {
+			MapNode* from = findNode(fromid);
+			MapNode* to = findNode(toid);
+			if(from == NULL || to == NULL)
+				return false;			
+			from->addEdge(toid);
+			to->addEdge(fromid);
+		}
+
+		void deleteNode(int id)
+		{
+			MapNode removee(id); 
+			nodeList.remove(removee);
+			purgeEdges(id);
+		}
+
+		void deleteNthNode(int n) {
+			MapNode *target = nodeList.getNth(n);
+			deleteNode(target->getId()); // this part is duplicating search to the target, might be inefficient..
+		}
+
+		bool deleteEdge(int fromid, int toid)
+		{
+			MapNode *from = findNode(fromid);
+			MapNode *to = findNode(toid);
+
+			if(from == NULL || to == NULL)
+				return false;
+
+			from->deleteEdge(toid);
+			to->deleteEdge(fromid);
+		}
+
+	protected:
 		SortedLinkedList<MapNode> nodeList;
+	private:
+
+		// delete all edges involving node with nodeid ( complexity: n * e)
+		void purgeEdges(int nodeid) {
+			ListIterator< MapNode> iter(nodeList);
+			for( MapNode *i = iter.start(); iter.hasNext(i); i = iter.next())
+			{
+				i->deleteEdge(nodeid);
+			}
+		}
 };
 
 
--- generation/list/LinkedList.h
+++ generation/list/LinkedList.h
@@ -3,10 +3,109 @@
 
 #include <ga/ga.h>
 
+template<typename T>
+class ListIterator : public GAListIter<T> {
+	public:
+
+	ListIterator(const GAList<T> &list)
+	: GAListIter<T>(list)
+	{
+		starting = true;
+		theHead = GAListIter<T>::head();
+	}
+
+	T *start()
+	{
+		return theHead;
+	}
+
+	bool isStart(T *i) {
+		return (theHead == i);
+	}
+
+	bool hasNext(T *i) {
+		bool rval = i && (starting || i!=theHead);
+		starting = false;
+		return rval;
+	}
+
+	T *next()
+	{
+		return GAListIter<T>::next();
+	}
+
+
+	private:
+		T *theHead;
+		bool starting;
+
+};
+
+
+
+
 template <typename T>
 class LinkedList : public GAList<T>
 {
 public:
+
+ 	T* find(const T& target)
+	{
+		ListIterator<T> iter(*this);
+		for(T *i = iter.start(); iter.hasNext(); i = iter.next())
+		{
+			if(target == *i)
+				return i;
+		}
+		return NULL;
+	}
+
+	T* getNth(int n)
+	{
+		ListIterator<T> iter(*this);
+		int j = 0;
+		for(T *i = 	iter.start(); iter.hasNext(); i = iter.next())
+		{
+			if(j == n)
+				return i;
+		}
+		return NULL;
+	}
+
+	bool remove(T& removee)
+	{
+		ListIterator<T> iter(*this);
+		for(T *i = 	iter.start(); iter.hasNext(); i = iter.next())
+		{
+			if(removee == *i) {
+				warp(iter);
+				GAList<T>::destroy();
+				return true;
+			}
+		}
+		return false;
+	}
+
+	bool removeNth(int n)
+	{
+		ListIterator<T> iter(*this);
+		int j = 0;
+		for(T *i = iter.start(); iter.hasNext(); i = iter.next())
+		{
+			if(j == n)
+			{
+				warp(iter);
+				GAList<T>::destroy();
+				return true;
+			}
+			if(j > n)
+				return false;
+			j++;
+		}
+		return false;
+		
+	}
+
 
 	friend ostream& operator<<(ostream &os, const LinkedList<T>& p) {
 		LinkedList<T> t(p);
@@ -20,9 +119,22 @@ public:
 
 /* sorted list that keeps unique elements only */
 template<typename T>
-class SortedLinkedList : public LinkedList
+class SortedLinkedList : public LinkedList<T>
 {
 	public: 
+
+ 	T* find(const T& target)
+	{
+		ListIterator<T> iter(*this);
+		for(T *i = iter.start(); iter.hasNext(); i = iter.next())
+		{
+			if(target == *i)
+				return i;
+			if(*i < target) // assuming sorted list, no need to go further
+				return NULL;
+		}
+		return NULL;
+	}
 
 	// insert T t with order
 	bool insert(const T& insertee)
@@ -36,70 +148,46 @@ class SortedLinkedList : public LinkedList
 			{
 				warp(iter);
 				if(iter.isStart(i))
-					LinkedList::insert(insertee, GAListBASE::HEAD);
+					LinkedList<T>::insert(insertee, GAListBASE::HEAD);
 				else
-					LinkedList::insert(insertee, GAListBASE::BEFORE);
+					LinkedList<T>::insert(insertee, GAListBASE::BEFORE);
 				return true;
 			}
 		}
 		
-		tail();
-		LinkedList::insert(insertee, GAListBASE::TAIL);
+		LinkedList<T>::tail();
+		LinkedList<T>::insert(insertee, GAListBASE::TAIL);
 	}
 
-	private:
-
-	void insert(GAList<T> * t, GAListBASE::Location where=AFTER)
+	bool remove(const T& removee)
 	{
-		LinkedList::insert(t, where);
-	}
-
-	void insert(const T& t, GAListBASE::Location where=AFTER)
-	{
-		LinkedList::insert(t, where);
-	}
-
-}
-
-
-
-template<typename T>
-class ListIterator : public GAListIter<T> {
-	public:
-
-	ListIterator<T>(const GAList<T> &list)
-	: GAListIter<T>(list)
-	{
-		start = true;
-		theHead = head();
-	}
-
-	T *start()
-	{
-		return theHead;
-	}
-
-	bool isStart(T *i) {
-		return (theHead == i);
-	}
-
-	bool hasNext(T *i) {
-		bool rval = i && (start || i!=theHead);
-		start = false;
-		return rval;
-	}
-
-	T *next()
-	{
-		return next();
+		ListIterator<T> iter(*this);
+		for(T *i = 	iter.start(); iter.hasNext(); i = iter.next())
+		{
+			if(removee == *i) {
+				LinkedList<T>::warp(iter);
+				LinkedList<T>::destroy();
+				return true;
+			}
+			if(*i < removee) // assuming sorted list, no need to go further
+				return false;
+		}
+		return false;	
 	}
 
 
 	private:
-		T *theHead;
-		bool start;
 
+	void insert(GAList<T> * t, GAListBASE::Location where=GAListBASE::AFTER)
+	{
+		LinkedList<T>::insert(t, where);
+	}
+/*
+	void insert(const T& t, GAListBASE::Location where=GAListBASE::AFTER)
+	{
+		LinkedList<T>::insert(t, where);
+	}
+*/
 };
-
 
 #endif
